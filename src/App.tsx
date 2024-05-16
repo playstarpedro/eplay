330441ef77326ccdaeb24c3801f55fa886ea95ef
pedroDev <playstarpedro@gmail.com>
2024-05-16 13:56:44 -0300

    componentes Hero e Product criados

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalCss } from './styles'
 import Header from './components/Header'
-
 import Rotas from './routes'
 import Footer from './components/Footer'
 
