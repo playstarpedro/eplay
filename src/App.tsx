754df4d95410fe97b5d9f9836c5aa693fd9da08e
pedroDev <playstarpedro@gmail.com>
2024-05-30 14:35:24 -0300

    Sidebar lateral criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalCss } from './styles'
@@ -5,16 +6,22 @@ import Header from './components/Header'
 import Rotas from './routes'
 import Footer from './components/Footer'
 
+import { store } from './store'
+import Cart from './components/Cart'
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+        <Cart />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
